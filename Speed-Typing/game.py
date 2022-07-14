1c916482b9c5ba5e2af4c1fdb7b2a8457792744f
WiserSquire <jacobdaniel0603@gmail.com>
2022-07-14 16:26:29 -0700

    Fullscreen mode Fixes #7

--- Speed-Typing/game.py
+++ Speed-Typing/game.py
@@ -1,42 +1,49 @@
 """File which includes the screen class and it's properties/methods"""
 
-import pygame
+import pygame as pg
 
 class Screen():
 
     def __init__(self):
-        pygame.init()
+        pg.init()
         self.width = 750
         self.height = 600
         self.run = True
         self.fullscreen = False
         self.fps = 60
-        self.fps_clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode(
-            (self.width, self.height), pygame.SCALED)
+        self.fps_clock = pg.time.Clock()
+        self.screen = pg.display.set_mode(
+            (self.width, self.height), pg.SCALED)
 
     def __del__(self):
-        pygame.quit()
+        pg.quit()
 
     def background(self, r=0, g=0, b=0):
         self.screen.fill((0, 0, 0))
 
     def update(self):
-        pygame.display.flip()
+        pg.display.flip()
         self.fps_clock.tick(self.fps)
 
-    def toggle_fullscreen():
-        pygame.display.toggle_fullscreen()
+    def set_mode(self, mode):
+        self.screen = pg.display.set_mode(
+            (self.width, self.height), mode)
+
+    def toggle_fullscreen(self):
+        pg.display.toggle_fullscreen()
 
 def game_loop():
     screen = Screen()
     while screen.run:
         screen.background()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
                 screen.run = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_F12:
+            if event.type == pg.KEYDOWN:
+                if (event.key == pg.K_F12 or
+                    (event.key == pg.K_ESCAPE and screen.fullscreen)):
+                    # If F12 is pressed or ESCAPE is pressed while window is in
+                    # fullscreen mode
                     screen.fullscreen = not screen.fullscreen
                     screen.toggle_fullscreen()
         screen.update()
